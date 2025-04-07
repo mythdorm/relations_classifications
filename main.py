dc292181cf311ae5033d0290558313f0b6a2dc2f
mythdorm <snunesj795@gmail.com>
2025-04-07 12:09:06 -0500

    Started adding the GUI elements for the app

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
 import relations
+import kivy
+from kivy.app import App
+from kivy.modules import inspector
+from kivy.core.window import Window
+
+class MatrixElement(Button):
+    def __init__(self, value=0, **kwargs):
+        super().__init__(**kwargs)
+        self.text = value
+
+class RelationsApp(App):
+    def build(self):
+        inspector.create_inspector(Window, self)
+
+    def create_matrix(self, text):
+        size = int(text)
+        matrix = []
+        for i in range(size):
+            matrix.append([])
+            for j in range(size):
+                matrix[i].append(0)
+        self.change_view(matrix)
+
+    def change_view(self, matrix):
+        for row in matrix:
+            for item in row:
+                pass
+
 
 def main():
     result = relations.check_reflexive([[1,0,0],[0,1,0],[0,0,1]])
@@ -37,3 +68,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    app = RelationsApp()
+    app.run()
